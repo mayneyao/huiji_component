ab14bf0208adc96248cbb51b819a773301c928ea
mayne <dreama9ain@gmail.com>
2018-11-21 17:35:40 +0800

    添加武器类型的详细描述

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -76,6 +76,52 @@ const typeMap = {
 	Weapon: '武器'
 };
 
+
+const ArmorTypeMap = {
+	Boots: '脚部',
+	Coat: '胸部',
+	Gloves: '手部',
+	Helm: '头部',
+	HelmAquatic: '水下呼吸器',
+	Leggings: '腿部',
+	Shoulders: '肩部'
+};
+
+const ArmorWeightClassMap = {
+	Heavy: '重甲',
+	Medium: '中甲',
+	Light: '轻甲',
+	Clothing: '城镇服装'
+};
+
+const ConsumableTypeMap = {
+	AppearanceChange: '外观改变',
+	Booze: '饮品',
+	ContractNpc: '联系NPC',
+	Food: '食物',
+	Generic: '各种消耗品',
+	Halloween: '增幅剂',
+	Immediate: '及时生效',
+	Transmutation: '皮肤幻化',
+	Unlock: '解锁',
+	UpgradeRemoval: '灌注拆解',
+	Utility: '磨刀石之类的',
+	TeleportToFriend: '好友传送'
+};
+
+const ConsumableUnlockTypeMap = {
+	BagSlot: '背包扩充',
+	BankTab: '银行栏位扩充',
+	CollectibleCapacity: '银行存储数量上限扩充',
+	Content: '解锁终结技,收藏成就,指挥官',
+	CraftingRecipe: '工艺配方',
+	Dye: '染料',
+	Outfit: '套装',
+	GliderSkin: '滑翔翼皮肤',
+	Champion: '迷雾冠军',
+	RandomUlock: '随机解锁'
+};
+
 const flagsMap = {
 	AccountBindOnUse: '使用后帐号绑定',
 	AccountBound: '帐号绑定',
@@ -121,10 +167,62 @@ const restrictionsMap = {
 	Warrior: '战士'
 };
 
+function WeaponDetail(props) {
+	const weaponDamageTypeMap = {
+		Fire:'火焰',
+		Ice:'寒冰',
+		Lightning:'光明',
+		Physical:'物理',
+		Choking:'没有用到'
+	};
+
+	const weaponTypeMap = {
+		Axe:'斧头',
+		Dagger:'匕首',
+		Pistol:'手枪',
+		Scepter:'节杖',
+		Sword:'单手剑',
+		Focus:'聚能器',
+		Shield:'盾牌',
+		Torch:'火炬',
+		Warhorn:'战号',
+		Greatsword:'大剑',
+		Hammer:'巨锤',
+		LongBow:'长弓',
+		Rifle:'步枪',
+		ShortBow:'短弓',
+		Staff:'法杖',
+		Harpoon:'鱼叉',
+		Speargun:'鱼枪',
+		Trident:'三叉戟',
+		LargeBundle:'LargeBundle',
+		SmallBundle:'SmallBundle',
+		Toy:'玩具',
+		TwoHandedToy:'TwoHandedToy'
+	};
+	const {type, damage_type, min_power, max_power, defense, infusion_slots, infix_upgrade, suffix_item_id, secondary_suffix_item_id} = props.data
+	return (
+		<div>
+			<div>
+				<span>武器类型:</span> <span>{weaponTypeMap[type]}</span>
+			</div>
+			<div>
+				<span>攻击力:</span> <span>{min_power}~{max_power}</span>
+			</div>
+			<div>
+				<span>伤害类型:</span> <span>{weaponDamageTypeMap[damage_type]}</span>
+			</div>
+			<div>
+				<span>防御力:</span> <span>{defense}</span>
+			</div>
+		</div>
+	)
+}
+
 function MediaCard(props) {
 	const {
 		classes, data: {
-			id, icon, name, name_en, description, rarity, type, level, vendor_value, flags, game_types, restrictions
+			id, icon, name, name_en, description, rarity, type, level, vendor_value, flags, game_types, restrictions, details
 		}
 	} = props;
 	return (
@@ -170,13 +268,19 @@ function MediaCard(props) {
 						                                                       className={classes.chip}/>)}</span>
 					</div>
 				}
+				<div>详细信息</div>
+				<hr/>
+				{
+					type === 'Weapon' && <WeaponDetail data={details}/>
+				}
+
 
 			</CardContent>
 			<CardActions>
 				<Button size="small" color="primary">
 					在中文维基中查看
 				</Button>
-				<Button size="small" color="primary">
+				<Button size="small" color="primary" href={`https://wiki.guildwars2.com/wiki/?search=${name_en}`}>
 					在英文维基上查看
 				</Button>
 				<Button size="small" color="primary" href={`http://www.gw2data.cn/itemdatabase/${id}`}>
